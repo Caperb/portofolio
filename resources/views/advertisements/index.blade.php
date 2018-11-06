8b0f816c4777bedd56f88f7c8efbf76a557c94d9
Casper Bosma <casperbosma@gmail.com>
2018-11-06 11:23:07 +0100

    Uploading Project

---
+++ resources/views/advertisements/index.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+        <div class="main_content">
+            <div class="grid-container">
+            <table>
+                <tr class="table_header">
+                    <th>Title:</th>
+                    <th>Description:</th>
+                </tr>
+                @foreach($advertisements as $advertisement)
+                    <tr>
+                        <div class="table_info">
+                        <td class="table_title">
+                            <a href="advertisements/{{ $advertisement->id }}">
+                                {{$advertisement->title}}
+                            </a>
+                        </td>
+                        <td>{{$advertisement->description}}</td>
+                        </div>
+
+                        <td>@role('verified-company')
+                            {{ Form::open(['method' => 'DELETE', 'route' => ['advertisements.destroy', $advertisement->id]]) }}
+                            {{ Form::submit('Delete', ['class' => 'delete_button']) }}
+                            {{ Form::close() }}
+                            @endrole
+                        </td>
+                        <td>@role('verified-company')
+                            {{ Form::open(['method' => 'GET', 'route' => ['advertisements.edit', $advertisement->id]]) }}
+                            {{ Form::submit('Edit', ['class' => 'edit_button']) }}
+                            {{ Form::close() }}
+                            @endrole
+                        </td>
+                    </tr>
+                @endforeach
+            </table>
+                @include('layouts.errors')
+            </div>
+            @role('verified-company')
+            <a href="advertisements/create">
+                {{ Form::submit('Create', ['class' => 'create_button']) }}
+            </a>
+            @endrole
+        </div>
+        <div class="back">
+            <a href="{{ url('/') }}" class="back_button">Back</a>
+        </div>
+@endsection
---
+++ resources/views/layouts/footer2.blade.php
@@ -0,0 +1,5 @@
+<link href="{{ asset('css/index.css') }}" rel="stylesheet">
+@section('footer')
+<div class="footer-copyright">© 2018 Copyright: Clickets!
+</div>
+    @endsection
